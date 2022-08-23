4c21d028b1763f19de25c18ee2f160ce3daf9c64
iyad-abdulwahid <iyadabdulwahid23@gmail.com>
2022-08-23 20:03:11 +0000

    got scripts to coexist, need to add titles and videos

--- scripts.js
+++
@@ -1,8 +0,0 @@
-var videoPlayer = document.getElementById("videoPlayer");
-var myVideo = document.getElementById("myVideo");
-
-function stopVideo(){
-    videoPlayer.style.display = 'none';
-}
-
-<><script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script></>
