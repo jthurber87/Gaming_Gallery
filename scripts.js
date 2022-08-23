02cf3a98540e0d5db50d3d88be47c3d036aec50b
iyad-abdulwahid <iyadabdulwahid23@gmail.com>
2022-08-23 06:46:10 +0000

    added new sections and css for each

--- scripts.js
+++ scripts.js
@@ -4,3 +4,5 @@ var myVideo = document.getElementById("myVideo");
 function stopVideo(){
     videoPlayer.style.display = 'none';
 }
+
+<><script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script></>
